3b6918800b2bda41eb1776d7a3cfba3ca7c85e34
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2024-05-14 02:15:58 +0800

    AR Object Placement Improvements
    Allows for ARTranslationInteractable to finally work

--- Assets/Scripts/Scanner/ARPlaceObject.cs
+++ Assets/Scripts/Scanner/ARPlaceObject.cs
@@ -1,22 +1,17 @@
 using Core;
 using DG.Tweening;
-using System.Collections.Generic;
 using UI;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.Interaction.Toolkit.AR;
 
 namespace Scanner
 {
     public class ARPlaceObject : MonoBehaviour
     {
-        [SerializeField] private ARRaycastManager raycastManager;
-        [SerializeField] private GameObject placementIndicator;
+        [SerializeField] private ARPlacementInteractable aRPlacementInteractable;
+        //[SerializeField] private GameObject placementIndicator;
         [SerializeField] private DialogueManager dialogueManager;
         [SerializeField] private VFXManager vfxManager;
-
-        [SerializeField] private InformationalData placeholderInfo;
-        [SerializeField] private DigeomonCaptureData digeomonCaptureData;
 
         [Header("SFX")]
         [SerializeField] private AudioClip summonSFX;
@@ -25,9 +20,7 @@ namespace Scanner
         private DigeomonData currDigeomon;
         private ScannerUI scannerUI;
         private GameObject spawnedObject;
-        private Pose placementPose;
-        private bool poseIsValid = false;
-        private bool isInitialized = false;
+        //private Pose placementPose;
 
         private void Awake()
         {
@@ -35,40 +28,43 @@ namespace Scanner
             scannerUI = GetComponent<ScannerUI>();
         }
 
-        private void Start()
-        {
-            placementIndicator.SetActive(false);
-        }
+        //private void Start()
+        //{
+        //    placementIndicator.SetActive(false);
+        //}
 
         private void OnEnable()
         {
             JournalManager.OnSummonAction += DirectSetupARObject;
+            aRPlacementInteractable.objectPlaced.AddListener(ObjectPlaced);
         }
 
         private void OnDisable()
         {
             JournalManager.OnSummonAction -= DirectSetupARObject;
+            aRPlacementInteractable.objectPlaced.RemoveAllListeners();
         }
 
-        private void Update()
-        {
-            if (isInitialized)
-            {
-                if (spawnedObject == null && poseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-                {
-                    PlaceObject();
-                }
+        //private void Update()
+        //{
+        //    if (isInitialized)
+        //    {
+        //        if (spawnedObject == null && poseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //        {
+        //            PlaceObject();
+        //        }
 
-                UpdatePlacePose();
-                UpdatePlacementIndicator();
-            }
-        }
+        //        UpdatePlacePose();
+        //        UpdatePlacementIndicator();
+        //    }
+        //}
 
         public void InitializeARObject(DigeomonData digeomon)
         {
+            //isInitialized = true;
             spawnedObject = null;
             currDigeomon = digeomon;
-            isInitialized = true;
+            aRPlacementInteractable.placementPrefab = digeomon.modelPrefab;
             scannerUI.ShowSummonHelp();
         }
 
@@ -78,40 +74,65 @@ namespace Scanner
             scannerUI.HideScanner();
         }
 
-        private void UpdatePlacementIndicator()
-        {
-            if (spawnedObject == null && poseIsValid)
-            {
-                placementIndicator.SetActive(true);
-                placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
-            }
-            else
-            {
-                placementIndicator.SetActive(false);
-            }
-        }
+        //private void UpdatePlacementIndicator()
+        //{
+        //    if (spawnedObject == null && poseIsValid)
+        //    {
+        //        placementIndicator.SetActive(true);
+        //        placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        //    }
+        //    else
+        //    {
+        //        placementIndicator.SetActive(false);
+        //    }
+        //}
 
-        private void UpdatePlacePose()
-        {
-            var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-            var hits = new List<ARRaycastHit>();
-            raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        //private void UpdatePlacePose()
+        //{
+        //    var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        //    var hits = new List<ARRaycastHit>();
+        //    raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
 
-            poseIsValid = hits.Count > 0;
-            if (poseIsValid)
-            {
-                placementPose = hits[0].pose;
-            }
-        }
+        //    poseIsValid = hits.Count > 0;
+        //    if (poseIsValid)
+        //    {
+        //        placementPose = hits[0].pose;
+        //    }
+        //}
 
-        private void PlaceObject()
-        {
-            isInitialized = false;
+        //private void PlaceObject()
+        //{
+        //    isInitialized = false;
             
-            spawnedObject = Instantiate(currDigeomon.modelPrefab, placementPose.position, placementPose.rotation);
+        //    spawnedObject = Instantiate(currDigeomon.modelPrefab, placementPose.position, placementPose.rotation);
+        //    spawnedObject.transform.localScale = Vector3.zero;
+        //    spawnedObject.transform.DOScale(1f, 0.5f).SetEase(Ease.OutQuint);
+            
+        //    audioSource.clip = summonSFX;
+        //    audioSource.Play();
+
+        //    vfxManager.InitializeDigeomon(spawnedObject, currDigeomon);
+
+        //    if (PersistentData.toSummon)
+        //    {
+        //        PersistentData.toSummon = false;
+        //        scannerUI.HideCaptureUI();
+        //        scannerUI.ShowScanner();
+        //        return;
+        //    }
+
+        //    scannerUI.ShowCaptureButton();
+        //    dialogueManager.StartDialogue(currDigeomon.introDialogue);
+        //}
+
+        private void ObjectPlaced(ARObjectPlacementEventArgs args)
+        {
+            aRPlacementInteractable.placementPrefab = null;
+
+            spawnedObject = args.placementObject;
             spawnedObject.transform.localScale = Vector3.zero;
             spawnedObject.transform.DOScale(1f, 0.5f).SetEase(Ease.OutQuint);
-            
+
             audioSource.clip = summonSFX;
             audioSource.Play();
 
